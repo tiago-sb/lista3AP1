37793e1c3061848d51bb9c75c9408702cae83804
tiago-sb <belatiago72@gmail.com>
2023-04-26 16:57:22 -0300

    Create questao01.cpp

---
+++ src/questao01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    system("cls");
+    if(m < n){
+        m++;
+        while(m < n){
+            cout << m << endl;
+            m++;
+        }
+    } else {
+        m--;
+        while(m > n){
+            cout << m << endl;
+            m--;
+        }
+    }
+
+    return 0;
+}
