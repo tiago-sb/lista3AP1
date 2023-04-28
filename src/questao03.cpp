64a98dadab522542c5f618a596c908ef96e3bbb8
tiago-sb <belatiago72@gmail.com>
2023-04-28 20:18:48 -0300

    Update questao03.cpp

--- src/questao03.cpp
+++ src/questao03.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main(){
-    setlocale(LC_ALL,"portuguese");
-    int v, a = 0, b = 1, aux;
-    cout << "Insira um valor: ";
+    int v, a = 0, b = 1, aux = 1;
     cin >> v;
     for(int i = 0;i < v;i++){
+        cout << aux << " ";
         aux = a + b;
         a = b;
         b = aux;
     }
-    cout << "sequência de Fibonnachi na posicao " << v << " é: " << aux << endl;
+
     return 0;
 }
