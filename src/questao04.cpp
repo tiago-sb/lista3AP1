134ed3b9ae999b4607cbad2f33f305eb642935bb
tiago-sb <belatiago72@gmail.com>
2023-04-28 20:19:00 -0300

    Update questao04.cpp

--- src/questao04.cpp
+++ src/questao04.cpp
@@ -1,8 +1,25 @@
+/*
+Escreva um programa que calcule a media das idades dos alunos de uma escola. Considere que
+o nemero de alunos nao eh conhecido a priori. Entao o seu algoritmo deve ler todas as idades
+até que um valor negativo seja fornecido.
+*/
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
+    setlocale(LC_ALL,"portuguese");
+    int idade, contador = 0;
+    float media, somaIdades = 0;
+    cin >> idade;
+    while(idade > 0){
+        somaIdades += idade;
+        cin >> idade;
+        contador++;
+    }
+    cout << fixed << setprecision(1);
+    cout << endl << "A média das idades é "<< somaIdades / contador;
 
     return 0;
 }
